88d75f754de640cae623c6fd78b1da5c025082a1
flopes777 <jakmarek654@gmail.com>
2025-10-25 20:41:40 +0200

    Add files via upload

---
+++ assets/user-data.js
@@ -0,0 +1,96 @@
+// assets/user-data.js
+export const userData = {
+    // Podstawowe dane
+    name: 'Jan',
+        surname: 'Pawlata',
+        nationality: 'polskie',
+        birthday: '09.08.2007',
+        pesel: '07280902957',
+        seriesAndNumber: 'ZZC 216937',
+        expiryDate: '15.03.2034',
+        givenDate: '15.03.2024',
+        fathersName: 'Artur',
+        mothersName: 'Renata',
+        familyName: 'Kowalski',
+        sex: 'M',
+        fathersFamilyName: 'Pawlata',
+        mothersFamilyName: 'Ledzion',
+        birthPlace: 'Łowicz',
+        countryOfBirth: 'Polska',
+        adress: 'ul. Ikara 2 , 99-400 Łowicz',
+        homeDate: '01.01.2010'
+};
+
+// Możesz też dodać różne profile użytkowników
+export const userProfiles = {
+    Admin: {
+        name: 'Jan',
+        surname: 'Pawlata',
+        nationality: 'polskie',
+        birthday: '09.08.2007',
+        pesel: '07280902957',
+        seriesAndNumber: 'ZZC 216937',
+        expiryDate: '15.03.2034',
+        givenDate: '15.03.2024',
+        fathersName: 'Artur',
+        mothersName: 'Renata',
+        familyName: 'Kowalski',
+        sex: 'M',
+        fathersFamilyName: 'Pawlata',
+        mothersFamilyName: 'Ledzion',
+        birthPlace: 'Łowicz',
+        countryOfBirth: 'Polska',
+        adress: 'ul. Ikara 2 , 99-400 Łowicz',
+        homeDate: '01.01.2010'
+    },
+    
+    Maja: {
+        name: 'Maja',
+        surname: 'Kosiorek',
+        nationality: 'polskie', 
+        birthday: '22.07.1990',
+        pesel: '90072212345',
+        seriesAndNumber: 'DEF789012',
+        expiryDate: '22.07.2035',
+        givenDate: '22.07.2025',
+        fathersName: 'Piotr',
+        mothersName: 'Maria',
+        familyName: 'Nowak',
+        sex: 'K',
+        fathersFamilyName: 'Nowak',
+        mothersFamilyName: 'Wiśniewska',
+        birthPlace: 'Kraków',
+        countryOfBirth: 'Polska',
+        adress: 'ul. Testowa 456, 30-001 Kraków',
+        homeDate: '15.05.2015'
+    },
+    
+    Filip: {
+        name: 'Filip',
+        surname: 'Żaczek',
+        nationality: 'polskie', 
+        birthday: '20.07.2007',
+        pesel: '07272002957',
+        seriesAndNumber: 'DEF789012',
+        expiryDate: '22.07.2035',
+        givenDate: '22.07.2025',
+        fathersName: 'Mariusz',
+        mothersName: 'Magdalena',
+        familyName: 'Żaczek',
+        sex: 'M',
+        fathersFamilyName: 'Żaczek',
+        mothersFamilyName: 'Słoma',
+        birthPlace: 'Skierniewice',
+        countryOfBirth: 'Polska',
+        adress: 'ul. Ikara 2, 99-400 Łowicz',
+        homeDate: '15.05.2017'
+    }
+};
+
+// Aktualnie wybrany profil (możesz zmienić)
+
+export const currentProfile = 'Admin'; // lub 'anna'
+
+
+
+
